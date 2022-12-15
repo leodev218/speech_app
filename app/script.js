2aeff3f864d306a8ea64425164f2665d7828d9e7
leodev218 <leonardo.dev218@gmail.com>
2022-12-15 17:38:54 -0500

    fist commit

---
+++ app/script.js
@@ -0,0 +1,4 @@
+document.forms["formulario"].onsubmit = function(){
+    var texto = document.getElementById("tituloOculto").value = document.getElementById("texto").innerHTML;
+    alert(texto);
+}
